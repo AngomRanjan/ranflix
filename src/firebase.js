21befdd6651292c4458565b803f6b783bb3f9965
AngomRanjan <angomranjan@gmail.com>
2023-01-11 15:23:34 +0530

    Add Firebase Config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkiO-SoD_OWCyjKcUnThpAi-CoXeJ8tok",
+  authDomain: "ranflix-f6810.firebaseapp.com",
+  projectId: "ranflix-f6810",
+  storageBucket: "ranflix-f6810.appspot.com",
+  messagingSenderId: "152303526014",
+  appId: "1:152303526014:web:40dff54e86d939824cb82f"
+};
