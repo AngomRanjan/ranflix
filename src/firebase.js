aca0e1455a26d306f67e86107e5afe976e00e30d
AngomRanjan <angomranjan@gmail.com>
2023-01-13 05:37:52 +0530

    Fix Eslint Errors

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAkiO-SoD_OWCyjKcUnThpAi-CoXeJ8tok",
-  authDomain: "ranflix-f6810.firebaseapp.com",
-  projectId: "ranflix-f6810",
-  storageBucket: "ranflix-f6810.appspot.com",
-  messagingSenderId: "152303526014",
-  appId: "1:152303526014:web:40dff54e86d939824cb82f"
+  apiKey: 'AIzaSyAkiO-SoD_OWCyjKcUnThpAi-CoXeJ8tok',
+  authDomain: 'ranflix-f6810.firebaseapp.com',
+  projectId: 'ranflix-f6810',
+  storageBucket: 'ranflix-f6810.appspot.com',
+  messagingSenderId: '152303526014',
+  appId: '1:152303526014:web:40dff54e86d939824cb82f',
 };
+
+export default firebaseConfig;
