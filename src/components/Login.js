2a468621b1d325a99270e22d1a784ce03113ea9b
AngomRanjan <angomranjan@gmail.com>
2023-01-25 21:34:39 +0530

    Add Login body

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,6 +15,21 @@ const Login = () => {
         <button type="button" className="login__button">Sign In</button>
         <div className="login__gradient" />
       </div>
+
+      <div className="login__body">
+        <>
+          <h1>Unlimited movies, TV shows and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+          <div className="login__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button type="button" className="login__getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
     </div>
   );
 };
