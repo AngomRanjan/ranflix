900c443dae0ea42a21b85ddd1f4bb1f85fd30c05
AngomRanjan <angomranjan@gmail.com>
2023-02-04 15:57:42 +0530

    Add test for userSlice

--- src/__test__/userReducer.test.js
+++
@@ -1,6 +0,0 @@
-import userReducer from '../features/userSlice';
-
-test('userReducer', () => {
-  const state = userReducer({ user: { user: null } }, { type: 'user/login', payload: { uid: 'bEDvhfarvoMVWXKtZIbhWMaAzxA3', email: 'b@b.com' } });
-  expect(state).toEqual({ user: { user: { uid: 'bEDvhfarvoMVWXKtZIbhWMaAzxA3', email: 'b@b.com' } } });
-});
---
+++ src/__test__/userSlice.test.js
@@ -0,0 +1,17 @@
+import userReducer from '../features/userSlice';
+
+describe(userReducer, () => {
+  const state = userReducer({ user: { user: null } }, { type: 'user/login', payload: { uid: 'bEDvhfarvoMVWXKtZIbhWMaAzxA3', email: 'b@b.com' } });
+
+  it("state equals current user", () => {
+    expect(state).toEqual({ user: { uid: 'bEDvhfarvoMVWXKtZIbhWMaAzxA3', email: 'b@b.com' } });
+  });
+
+  it('check state.user.uid to be bEDvhfarvoMVWXKtZIbhWMaAzxA3', () => {
+    expect(state.user.uid).toEqual('bEDvhfarvoMVWXKtZIbhWMaAzxA3');
+  });
+  
+  it('check state.user.email to equal b@b.com', () => {
+    expect(state.user.email).toEqual('b@b.com');
+  });
+});
