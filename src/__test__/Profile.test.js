81e9777484f7d1565605606667699176c0327288
AngomRanjan <angomranjan@gmail.com>
2023-02-07 11:55:15 +0530

    Add Test for Profile

---
+++ src/__test__/Profile.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Profile from "../components/Profile";
+
+describe(Profile, () => {
+  it('profile render correctly', () => {
+    render(<Profile />)
+    const textEle = screen.getByText('Profile');
+    expect(textEle).toBeInTheDocument;
+  });
+});
